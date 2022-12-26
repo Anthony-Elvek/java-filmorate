e28cb6fd651b5c93f389837aea2349974f535ec0
Anthony-Elvek <morty08@yandex.ru>
2022-12-27 00:02:14 +0300

    Исправлены замечания

--- src/main/java/ru/yandex/practicum/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/controller/ErrorHandler.java
@@ -1,32 +1,41 @@
 package ru.yandex.practicum.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.exception.NotFoundException;
 import ru.yandex.practicum.exception.ValidationException;
 import ru.yandex.practicum.model.ErrorResponse;
 
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorResponse> handlerNotFoundException(final NotFoundException e) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .build();
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handlerNotFoundException(final NotFoundException e) {
+        log.info("Not found exception...");
+        return new ErrorResponse(
+                "Not found exception"
+        );
     }
 
-    @ExceptionHandler(Throwable.class)
-    public ResponseEntity<ErrorResponse> handlerThrowableException(final Throwable e) {
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .build();
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(final Throwable e) {
+        log.info("Internal server error");
+        return new ErrorResponse(
+                "Internal server error"
+        );
     }
 
-    @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<ErrorResponse> handlerValidationException(final ValidationException e) {
+    @ExceptionHandler({ValidationException.class, MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorResponse> handlerValidationException(final MethodArgumentNotValidException e) {
+        log.info("Validation exception...");
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .build();
